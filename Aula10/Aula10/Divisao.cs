b953cf2ab67002f648980d95f5daa007262d4786
igorsantos <igorsantos@ODADSK0018>
2017-05-17 14:43:49 -0300

    Class Divisão

---
+++ Aula10/Aula10/Divisao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula10
+{
+    class Divisao : Operacao
+    {
+
+        public override double FazAconta(double n1, double n2)
+        {
+            return n1 / n2;
+        }
+
+    }
+}
--- Aula10/Aula10/Program.cs
+++ Aula10/Aula10/Program.cs
@@ -13,6 +13,7 @@ namespace Aula10
             Console.WriteLine("");
             Console.WriteLine("0.Soma");
             Console.WriteLine("1.Subtração");
+            Console.WriteLine("2.Divisão");
 
 
         }
@@ -21,10 +22,11 @@ namespace Aula10
         static void Main(string[] args)
         {
 
-            Operacao[] operacoes = new Operacao[2];
+            Operacao[] operacoes = new Operacao[3];
 
            operacoes [0] = new Soma();
            operacoes [1] = new Subtracao();
+           operacoes [2] = new Divisao();
 
 
             Console.WriteLine("Digite um número");
